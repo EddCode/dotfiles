ff6ecaf6794e0244f8119ac86d487f6e4051db2a
Edgar Figueroa <edgar.figueroa.gtz@gmail.com>
2020-05-17 18:07:38 -0500

    Create scripts for install config files

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ source brew/brew.sh
 echo "Moving config.fish"
 ln -sf ./fish/config.fish $HOME/.config/fish/config.fish
 echo "Movin fish_prompt_config.fish"
-ln -s ./fish/fish_propmpt.fish $HOME/.config/fish/functions/fish_prompt.fish:w
+ln -s ./fish/fish_propmpt.fish $HOME/.config/fish/functions/fish_prompt.fish
 
 # Cloning vim config & plugins
 echo "Cloning vim repositories"
