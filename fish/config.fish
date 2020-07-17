8303e1e0b56fe0480d97e13d30f84ad4ccdcc026
Edgar Figueroa <edgar.figueroa.gtz@gmail.com>
2020-07-17 16:56:58 -0500

    Add fishconfig gopaath

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,11 @@
 # ----GENERAL
 set -x EDITOR vim
 set -x LC_ALL en_US.UTF-8
+
 # ----NODE
 set -x NODE_OPTIONS --max_old_space_size=4096
+
+# --- GO
+set -gx GOPATH $HOME/Proyects/go
 
 set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" $fish_user_paths
