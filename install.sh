9e13166b3f87adcd01693a5b7f130df017994854
Edgar Figueroa <edgar.figueroa.gtz@gmail.com>
2024-11-05 11:49:03 -0600

    install updates

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ source brew/brew.sh
 
 brew bundle
 
+echo PATH="$PATH=/opt/homebrew/bin" >> .zshrc
+
 # Cloning vim config & plugins
 echo " ********* Cloning vim repositories ********* "
 git clone --recursive https://github.com/EddCode/vimrc.git ~/.dotvim
@@ -15,10 +17,11 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 launchctl load -w ~/Library/LaunchAgents/io.arslan.dark-mode-notify.plist
 
 echo " ********* Setting up fish and fisher ********* "
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-fisher install IlanCosman/tide@v6
+fish | curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fish | fisher install IlanCosman/tide@v6
+
 sudo chsh -s /opt/homebrew/bin/fish
-fish_add_path /opt/homebrew/bin
+fish | fish_add_path /opt/homebrew/bin
 
 # Setup tide prompt
 tide configure
