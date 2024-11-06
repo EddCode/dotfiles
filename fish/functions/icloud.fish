bfac092e66901c67b9579b3b72165e91ffd8f01b
Edgar Figueroa <edgar.figueroa.gtz@gmail.com>
2024-11-05 23:32:37 -0600

    delete function folderRemove tide fish functions

--- fish/functions/icloud.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function icloud --wraps=cd\ \~/Library/Mobile\\\ Documents/com\~apple\~CloudDocs/ --description alias\ icloud=cd\ \~/Library/Mobile\\\ Documents/com\~apple\~CloudDocs/
-  cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/ $argv;
-end
